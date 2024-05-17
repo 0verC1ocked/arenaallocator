24702525e4bcc1c80e0c784e48083bb4dff03dbd
Rohan-Muslekar <rohnabhi20vt@gmail.com>
2024-05-17 15:12:08 +0530

    chore: pb lib

---
+++ payload-builder.cpp
@@ -0,0 +1,57 @@
+#include "payload-builder.h"
+
+PayloadBuilder::PayloadBuilder() {
+    this->_arena = std::make_unique<google::protobuf::Arena>();
+}
+
+PayloadBuilder::~PayloadBuilder() {
+    this->_arena.reset();
+}
+
+PayloadBuilder& PayloadBuilder::setEvent(PAYLOAD::Events event) {
+    this->_event = event;
+    return *this;
+}
+
+PayloadBuilder& PayloadBuilder::reset() {
+    this->_event = PAYLOAD::Events::NONE;
+    this->_uid = "";
+    this->_rid = "";
+    this->_oid = "";
+    this->_msg = "";
+    return *this;
+}
+
+PayloadBuilder& PayloadBuilder::setUserId(std::string UID) {
+    this->_uid = UID;
+    return *this;
+}
+
+PayloadBuilder& PayloadBuilder::setRoomId(std::string RID) {
+    this->_rid = RID;
+    return *this;
+}
+
+PayloadBuilder& PayloadBuilder::setOpponentId(std::string OID) {
+    this->_oid = OID;
+    return *this;
+}
+
+PayloadBuilder& PayloadBuilder::setMessage(std::string MSG) {
+    this->_msg = MSG;
+    return *this;
+}
+
+PAYLOAD::Payload* PayloadBuilder::newPayload() {
+    return google::protobuf::Arena::CreateMessage<PAYLOAD::Payload>(this->_arena.get());
+}
+
+PayloadBuilder& PayloadBuilder::build(PAYLOAD::Payload* payload) {
+    payload->set_event(this->_event);
+    payload->mutable_data()->set_userid(this->_uid);
+    payload->mutable_data()->set_opponentid(this->_oid);
+    payload->mutable_data()->set_roomid(this->_rid);
+    payload->mutable_data()->set_msg(this->_msg);
+    this->reset();
+    return *this;
+}
---
+++ payload-builder.h
@@ -0,0 +1,31 @@
+#pragma once 
+
+#include "proto/payload.pb.h"
+#include <google/protobuf/arena.h>
+
+class PayloadBuilder {
+public:
+    PayloadBuilder();
+    ~PayloadBuilder();
+
+    PayloadBuilder& setEvent(PAYLOAD::Events event);   
+    PayloadBuilder& setUserId(std::string UID);
+    PayloadBuilder& setRoomId(std::string RID);
+    PayloadBuilder& setOpponentId(std::string OID);
+    PayloadBuilder& setMessage(std::string MSG);
+    
+
+    PayloadBuilder& reset();
+    PAYLOAD::Payload* newPayload();
+    PayloadBuilder& build(PAYLOAD::Payload* payload);
+
+private:
+    std::string _uid = "";
+    std::string _rid = "";
+    std::string _oid = "";
+    std::string _msg = "";
+    PAYLOAD::Events _event = PAYLOAD::Events::NONE;
+
+    std::unique_ptr<google::protobuf::Arena> _arena;
+
+};
